2869a34c0cd4575c15e8f20fee2d76544fe2941a
wesmith <smiwarsky@gmail.com>
2021-12-17 05:27:06 -0700

    first RPi assembly-code example

---
+++ ch6/prog6a.s
@@ -0,0 +1,16 @@
+/* prog6a.s from Bruth Smith book,
+	WS 12/17/21
+	perform R0 = R1 + R2*/
+
+	.global _start
+
+_start:
+
+	mov R1, #50
+	mov R2, #60
+	adds R0, R1, R2  @ compiler didn't like a space before 's'
+
+	mov R7, #1   @ exit through syscall
+	swi 0
+	
+	
